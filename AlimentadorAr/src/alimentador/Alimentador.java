90c99651106c7336d5668cba0a3d49521d6725bb
arenasoy <caro.arenas2607@gmail.com>
2019-06-18 21:50:01 +0000

    Add alimentador ar methods

--- AlimentadorAr/src/alimentador/Alimentador.java
+++ AlimentadorAr/src/alimentador/Alimentador.java
@@ -1,5 +1,115 @@
 package alimentador;
 
-public class Alimentador {
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class Alimentador {
+	
+	private String server;
+	private int port = 9000;
+	private int id = 0;
+	private Socket socket;
+	private OutputStreamWriter osw;
+	private BufferedWriter bw;
+	
+	public void connect() {
+		try {
+			System.out.println("connecting to " + server);
+			startBuffers();
+			bw.write("SALA_INTEL\n");
+			bw.write("TYPE: AR_CONDICIONADO\n");
+			bw.write("ID: " + id++ + "\n");
+			bw.write("LEN: " + 0 + "\n");
+			bw.write("ACTION: CONNECT\n");
+			
+			closeBuffers();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void startBuffers() throws UnknownHostException, IOException,
+			UnsupportedEncodingException {
+		socket = new Socket(server, port);
+		osw = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
+		bw = new BufferedWriter(osw);
+	}
+	
+	public void answerChange(String action, char result) {
+		try {
+			startBuffers();
+			
+			bw.write("SALA_INTEL\n");
+			bw.write("TYPE: AR_CONDICIONADO\n");
+			bw.write("ID: " + id++ + "\n");
+			bw.write("LEN: " + Character.BYTES + "\n");
+			bw.write("ACTION: " + action + "\n" );
+			bw.write(result + "\n");
+			
+			closeBuffers();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void answerGetTemperature(int temperature) {
+		try {
+			startBuffers();
+			
+			bw.write("SALA_INTEL\n");
+			bw.write("TYPE: AR_CONDICIONADO\n");
+			bw.write("ID: " + id++ + "\n");
+			bw.write("LEN: " + Integer.BYTES + "\n");
+			bw.write("ACTION: GET_TEMP\n" );
+			bw.write(temperature + "\n");
+			
+			closeBuffers();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void answerSetTemperature(char result) {
+		try {
+			startBuffers();
+			
+			bw.write("SALA_INTEL\n");
+			bw.write("TYPE: AR_CONDICIONADO\n");
+			bw.write("ID: " + id++ + "\n");
+			bw.write("LEN: " + Character.BYTES + "\n");
+			bw.write("ACTION: SET_TEMP\n" );
+			bw.write(result + "\n");
+			
+			closeBuffers();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void closeBuffers() throws IOException {
+		bw.flush();
+		bw.close();
+		osw.close();
+		socket.close();
+	}
+
+	public String getServer() {
+		return server;
+	}
+
+	public void setServer(String server) {
+		this.server = server;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
 }
--- AlimentadorAr/src/main/Main.java
+++ AlimentadorAr/src/main/Main.java
@@ -1,10 +1,29 @@
 package main;
 
+import alimentador.Alimentador;
+import console.Console;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Console console = Console.getInstance();
+		Alimentador alimentador = new Alimentador();
+		while (true) {
+		
+			int read = console.readInt("1 - connect\n2 - answer status\n3 - answer get temperature\n4 - answer set temperature");
+			
+			if (read == 1) {
+				//TODO if is connected continue
+				alimentador.setServer(console.readLine("Server IP: "));
+				alimentador.connect();
+			} else if (read == 2) {
+				alimentador.answerChange(console.readString("Action: "), console.readChar("Result: "));
+			} else if (read == 3) {
+				alimentador.answerGetTemperature(console.readInt("Actual temperature: "));
+			} else if (read == 4) {
+				alimentador.answerSetTemperature(console.readChar("1 - Success\n0 - Error"));
+			}
+		}
 	}
 
 }
