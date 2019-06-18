59f2ad7a5ae52849fc9fb5bf3c8833ed9bc5cd29
arenasoy <caro.arenas2607@gmail.com>
2019-06-18 20:47:04 +0000

    Add iluminacao methods

--- SistemaIluminacao/src/main/Main.java
+++ SistemaIluminacao/src/main/Main.java
@@ -1,9 +1,25 @@
 package main;
 
+import sistema.Iluminacao;
+import console.Console;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		Console console = Console.getInstance();
+		Iluminacao iluminacao = new Iluminacao();
+		while (true) {
+		
+			int read = console.readInt("1 - connect\n2 - answer connect");
+			
+			if (read == 1) {
+				//TODO if is connected continue
+				iluminacao.setServer(console.readLine("Server IP: "));
+				iluminacao.connect();
+			} else if (read == 2) {
+				iluminacao.answerChange(console.readString("Action: "), console.readChar("Result: "));
+			}
+		}
 	}
 
 }
--- SistemaIluminacao/src/sistema/Iluminacao.java
+++ SistemaIluminacao/src/sistema/Iluminacao.java
@@ -1,5 +1,73 @@
 package sistema;
 
-public class Iluminacao {
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 
+public class Iluminacao {
+	
+	private String server;
+	private int port = 9000;
+	private int id = 0;
+	private Socket socket;
+	private OutputStreamWriter osw;
+	private BufferedWriter bw;
+	
+	public void connect() {
+		try {
+			System.out.println("connecting to " + server);
+			socket = new Socket(server, port);
+			osw = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
+			bw = new BufferedWriter(osw);
+			bw.write("SALA_INTEL\n");
+			bw.write("TYPE: SISTEMA_ILUMINACAO\n");
+			bw.write("ID: " + id++ + "\n");
+			bw.write("LEN: " + 0 + "\n");
+			bw.write("ACTION: CONNECT\n");
+			
+			bw.flush();
+			bw.close();
+			osw.close();
+			socket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void answerChange(String action, char result) {
+		try {
+			socket = new Socket(server, port);
+			osw = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
+			bw = new BufferedWriter(osw);
+			bw.write("SALA_INTEL\n");
+			bw.write("TYPE: SISTEMA_ILUMINACAO\n");
+			bw.write("ID: " + id++ + "\n");
+			bw.write("LEN: " + Character.SIZE + "\n");
+			bw.write("ACTION: " + action + "\n" );
+			bw.write(result + "\n");
+			
+			bw.flush();
+			bw.close();
+			osw.close();
+			socket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getServer() {
+		return server;
+	}
+
+	public void setServer(String server) {
+		this.server = server;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
 }
